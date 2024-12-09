12c9ed49b9bb3a7f54151fb73170d56a798bbbab
Creastv <creastv@gail.com>
2024-12-09 11:04:27 +0100

    init

--- view-item.php
+++ view-item.php
@@ -1,6 +1,7 @@
 <?php
-// Hook do WooCommerce
-add_action('wp_footer', 'wc_datalayer_view_item');
+if (!defined('ABSPATH')) {
+    exit; // Bezpośredni dostęp zabroniony
+}
 
 function wc_datalayer_view_item()
 {
@@ -24,8 +25,7 @@ function wc_datalayer_view_item()
                     'item_name' => $product->get_name(),
                     'item_id' => $product->get_sku() ?: $product->get_id(),
                     'price' => $product->get_price(),
-                    // 'item_brand' => wc_datalayer_get_product_brand($product),
-                    'item_brand'  => 'Bini',
+                    'item_brand' => 'Bini', // Domyślna marka
                     'item_category' => wc_get_product_category_list($product->get_id(), ', '),
                     'item_variant' => '',
                     'quantity' => 1, // Domyślnie 1 dla view_item
@@ -40,12 +40,3 @@ function wc_datalayer_view_item()
     echo 'window.dataLayer.push(' . json_encode($product_data) . ');';
     echo '</script>';
 }
-
-function wc_datalayer_get_product_brand($product)
-{
-    // Funkcja pomocnicza do pobrania marki produktu
-    $brand_terms = get_the_terms($product->get_id(), 'product_brand');
-    return $brand_terms && !is_wp_error($brand_terms) ? $brand_terms[0]->name : '';
-}
-
-add_action('wp_footer', 'wc_datalayer_view_item');
--- woocommerce-datalayer.php
+++ woocommerce-datalayer.php
@@ -12,3 +12,9 @@ Text Domain: woocommerce-datalayer
 if (!defined('ABSPATH')) {
     exit; // Bezpośredni dostęp zabroniony
 }
+
+// Wczytanie pliku z kodem Data Layer
+require_once plugin_dir_path(__FILE__) . 'view-item.php';
+
+// Hook do WooCommerce
+add_action('wp_footer', 'wc_datalayer_view_item');
